8ed70a39601840b25721041c1b029d1ef2301ed5
Krzysztof Platis <platonn.git@gmail.com>
2023-06-30 15:40:32 +0200

    Revert "isr: change hardcoded URL for ISR"
    This reverts commit 41e5469104dbaef53becd854a854cc94f0a4a6ec.

--- vercel-output.js
+++ vercel-output.js
@@ -100,8 +100,8 @@ write(
     routes: [
       // Specify that ISR should be used for a product detail page
       {
-        src: "/electronics-spa/en/USD/product/300938/Photosmart%20E317%20Digital%20Camera$",
-        dest: "/isr-electronics-detail-page?__pathname=/electronics-spa/en/USD/product/300938/Photosmart%20E317%20Digital%20Camera",
+        src: "/electronics-spa/en/USD/product/358639/DSC-N1$",
+        dest: "/isr-electronics-detail-page?__pathname=/electronics-spa/en/USD/product/358639/DSC-N1",
       },
       // Specify that SSR should be used for all other pages
       {
